f655dd86c2f50cd25b1b14825d3ce4e4bc350981
LunaUI <59039458+LunaValerie@users.noreply.github.com>
2023-08-06 02:39:22 -0800

    pro damage
    pro damage

---
+++ Prodamage.cpp
@@ -0,0 +1,24 @@
+#pragma once
+typedef D3DXVECTOR3 WaitHack;
+DWORD SendBattleAttackPacketCall = 0x00CFC2E0; // Send Battle Attack Packet > Call
+DWORD CPpythonNetworkSteam = *(DWORD*)0xF87474; // CPpythonNetworkSteam > String
+
+
+typedef void(__thiscall* sendattack)(DWORD cpythonnetworkstream, UINT attack, DWORD vid, const WaitHack& m_iVecOrigin);
+sendattack SendBattleAttackPacket = (sendattack)SendBattleAttackPacketCall;
+
+bool dwProDamage17 = false;
+float dwSpeed17 = 100.0f;
+
+DWORD WINAPI x17(LPVOID lpParam)
+{
+	while (true)
+	{
+		if (dwProDamage17)
+		{
+			DWORD GetTargetVID = *reinterpret_cast<DWORD*>(*reinterpret_cast<DWORD*>(0x00F8BE94) + (0x00033FE4)); WaitHack dwVecOrigin; // GetTargetVID - OnCannotShot
+			SendBattleAttackPacket(CPpythonNetworkSteam, 0, GetTargetVID, dwVecOrigin);
+		}
+		Sleep(dwSpeed17);
+	}
+}
