fbe9260ae1079e6fb6a930b8c0ec55668156970f
LunaUI <59039458+LunaValerie@users.noreply.github.com>
2023-08-06 02:39:55 -0800

    Update Prodamage.cpp

--- Prodamage.cpp
+++ Prodamage.cpp
@@ -16,7 +16,7 @@ DWORD WINAPI x17(LPVOID lpParam)
 	{
 		if (dwProDamage17)
 		{
-			DWORD GetTargetVID = *reinterpret_cast<DWORD*>(*reinterpret_cast<DWORD*>(0x00F8BE94) + (0x00033FE4)); WaitHack dwVecOrigin; // GetTargetVID - OnCannotShot
+			DWORD GetTargetVID = *reinterpret_cast<DWORD*>(*reinterpret_cast<DWORD*>(0x00F8BE94) + (0x00033FE4)); WaitHack dwVecOrigin; // GetTargetVID
 			SendBattleAttackPacket(CPpythonNetworkSteam, 0, GetTargetVID, dwVecOrigin);
 		}
 		Sleep(dwSpeed17);
